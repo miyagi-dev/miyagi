e2f2eedc1d098038626203239fdf0c6f111c3e5a
mgrsskls <mail@mgrossklaus.de>
2019-06-18 16:10:07 +0200

    wip adding html validation to components

--- assets/js/iframe.js
+++ assets/js/iframe.js
@@ -4,89 +4,197 @@ function escapeHtml(str) {
   return div.innerHTML;
 }
 
-addEventListener("DOMContentLoaded", () => {
-  if (document.getElementById("Pattern")) {
-    const states = ["passes", "inapplicable", "violations", "incomplete"];
-    const summaries = Array.from(
-      document.querySelectorAll(".A11yResults-summary")
-    );
+function a11yTest() {
+  const states = ["passes", "inapplicable", "violations", "incomplete"];
+  const container = document.querySelector(".A11yResultsContainer--a11y");
+  const summaries = Array.from(
+    container.querySelectorAll(".A11yResults-summary")
+  );
 
-    summaries.forEach(summary => {
-      summary.addEventListener("click", e => {
-        summaries.forEach(sum => {
-          const details = sum.closest("details");
-          if (e.target.closest("details") !== details) {
-            if (details.open) {
-              sum.click();
-            }
+  summaries.forEach(summary => {
+    summary.addEventListener("click", e => {
+      summaries.forEach(sum => {
+        const details = sum.closest("details");
+        if (e.target.closest("details") !== details) {
+          if (details.open) {
+            sum.click();
           }
-        });
+        }
       });
     });
+  });
 
-    axe.run(document.getElementById("Pattern"), function(err, results) {
-      if (err) throw err;
+  axe.run(document.getElementById("Pattern"), function(err, results) {
+    if (err) throw err;
 
-      states.forEach(state => {
-        let html = "";
+    states.forEach(state => {
+      const resultElement = container.querySelector(
+        `.A11yResults--${state} .A11yResults-result`
+      );
+      let html = "";
 
-        document.querySelector(
-          `.A11yResults--${state} .A11yResults-result`
-        ).innerText = results[state].length;
+      resultElement.innerText = results[state].length;
 
-        if (results[state].length) {
-          html += '<ul class="A11yResults-entries">';
-          results[state].forEach(result => {
-            html += '<li class="A11yResults-entry">';
-            html += "<dl>";
+      if (
+        (state === "violations" || state === "incomplete") &&
+        results[state].length
+      ) {
+        resultElement.classList.add("has-positiveValue");
+      }
 
-            if (result.helpUrl) {
-              html += `<dt>Description:</dt> <dd>${escapeHtml(
-                result.description
-              )}</dd>`;
+      if (results[state].length) {
+        html += '<ul class="A11yResults-entries">';
+        results[state].forEach(result => {
+          html += '<li class="A11yResults-entry">';
+          html += '<dl class="A11yResults-data">';
+
+          if (result.description) {
+            html += `<dt>Description</dt> <dd>${escapeHtml(
+              result.description
+            )}</dd>`;
+          }
+
+          if (result.help) {
+            html += `<dt>Help</dt> <dd>${escapeHtml(result.help)}</dd>`;
+          }
+
+          if (result.helpUrl) {
+            html += `<dt>Link</dt> <dd><a href="${
+              result.helpUrl
+            }" target="_blank">${result.helpUrl}</dd></a>`;
+          }
+
+          if (result.impact) {
+            let impactClass = "";
+
+            switch (result.impact) {
+              case "serious":
+                impactClass = "is-negative";
+                break;
+              case "moderate":
+                impactClass = "is-warning";
             }
 
-            if (result.helpUrl) {
-              html += `<dt>Help:</dt> <dd>${escapeHtml(result.help)}</dd>`;
-            }
+            html += `<dt>Impact</dt> <dd class="${impactClass}">${
+              result.impact
+            }</dd>`;
+          }
 
-            if (result.helpUrl) {
-              html += `<dt>Link:</dt> <dd><a href="${
-                result.helpUrl
-              }" target="_blank">${result.helpUrl}</dd></a>`;
-            }
+          html += "</dl>";
+          html += "</li>";
+        });
+        html += "</ul>";
+      } else {
+        html +=
+          '<p><i class="A11yResults-noResults">Nothing to report.</i></p>';
+      }
 
-            if (result.impact) {
-              let impactClass = "";
+      container.querySelector(
+        `.A11yResults--${state} .A11yResults-details`
+      ).innerHTML = html;
+    });
 
-              switch (result.impact) {
-                case "serious":
-                  impactClass = "is-negative";
-                  break;
-                case "moderate":
-                  impactClass = "is-warning";
+    container.removeAttribute("hidden");
+  });
+}
+
+function htmlTest() {
+  const container = document.querySelector(".A11yResultsContainer--html");
+  const states = ["error", "warning"];
+
+  fetch(location.href).then(response => {
+    if (response.ok) {
+      response.text().then(html => {
+        const formData = new FormData();
+        formData.append("out", "json");
+        formData.append("content", html);
+        fetch("http://html5.validator.nu/", {
+          method: "post",
+          headers: {
+            Accept: "application/json"
+          },
+          body: formData
+        })
+          .then(function(response) {
+            return response.json();
+          })
+          .then(function(data) {
+            states.forEach(state => {
+              const results = data.messages.filter(
+                message => message.type === state
+              );
+              const resultElement = container.querySelector(
+                `.A11yResults--${state} .A11yResults-result`
+              );
+              let html = "";
+
+              resultElement.innerText = results.length;
+
+              if (results.length) {
+                resultElement.classList.add("has-positiveValue");
               }
 
-              html += `<dt>Impact:</dt> <dd class="${impactClass}">${
-                result.impact
-              }</dd>`;
-            }
+              if (results.length) {
+                html += '<ul class="A11yResults-entries">';
+                results.forEach(result => {
+                  html += '<li class="A11yResults-entry">';
+                  html += '<dl class="A11yResults-data">';
 
-            html += "</dl>";
-            html += "</li>";
+                  if (result.message) {
+                    html += `<dt>Message</dt> <dd>${result.message}</dd>`;
+                  }
+
+                  if (result.extract) {
+                    const markedExtract = `${escapeHtml(
+                      result.extract.slice(0, result.hiliteStart)
+                    )}<mark>${escapeHtml(
+                      result.extract.slice(
+                        result.hiliteStart,
+                        result.hiliteStart + result.hiliteLength
+                      )
+                    )}</mark>${escapeHtml(
+                      result.extract.slice(
+                        result.hiliteStart + result.hiliteLength
+                      )
+                    )}`;
+
+                    html += `<dt>Extract</dt> <dd><code class="extract">${markedExtract.replace(
+                      /\n/g,
+                      "↩"
+                    )}</code></dd>`;
+                  }
+
+                  html += `<dt>From</dt><dd>Line: ${
+                    result[result.firstLine ? "firstLine" : "lastLine"]
+                  }, Column: ${result.firstColumn}</dd>`;
+                  html += `<dt>To</dt><dd>Line: ${result.lastLine}, Column: ${
+                    result.lastColumn
+                  }</dd>`;
+
+                  html += "</dl>";
+                  html += "</li>";
+                });
+                html += "</ul>";
+              } else {
+                html +=
+                  '<p><i class="A11yResults-noResults">Nothing to report.</i></p>';
+              }
+
+              container.querySelector(
+                `.A11yResults--${state} .A11yResults-details`
+              ).innerHTML = html;
+            });
+
+            container.removeAttribute("hidden");
           });
-          html += "</ul>";
-        } else {
-          html +=
-            '<p><i class="A11yResults-noResults">Nothing to report.</i></p>';
-        }
-
-        document.querySelector(
-          `.A11yResults--${state} .A11yResults-details`
-        ).innerHTML = html;
       });
+    }
+  });
+}
 
-      document.querySelector(".A11yResultsContainer").removeAttribute("hidden");
-    });
+addEventListener("DOMContentLoaded", () => {
+  if (document.getElementById("Pattern")) {
+    a11yTest();
+    htmlTest();
   }
 });
