37c5ddb05e38fb40c89ecf1bc0ab16470f2a4b28
mgrsskls <mail@mgrossklaus.de>
2019-06-19 19:36:29 +0200

    refactor registerPartials

--- src/hbs/registerPartials.js
+++ src/hbs/registerPartials.js
@@ -2,21 +2,29 @@ const fs = require("fs");
 const path = require("path");
 const { pathEndsWithExtension } = require("../extensionHelper.js");
 
+function register(hbs, name, fullFilePath) {
+  let readFile;
+
+  try {
+    readFile = fs.readFileSync(fullFilePath, "utf8");
+  } catch (e) {
+    console.warn(
+      `Couldn't find file ${fullFilePath}. Is the 'srcFolder' in your styleguide.json correct?`
+    );
+  }
+
+  if (readFile) {
+    hbs.registerPartial(name, hbs.compile(readFile.toString()));
+  }
+}
+
 module.exports = function(app, hbs, initial) {
   if (initial) {
-    hbs.registerPartial(
-      "main",
-      fs.readFileSync(
-        path.join(__dirname, "../../views/layouts/main.hbs"),
-        "utf8"
-      )
-    );
-    hbs.registerPartial(
+    register(hbs, "main", path.join(__dirname, "../../views/layouts/main.hbs"));
+    register(
+      hbs,
       "iframe",
-      fs.readFileSync(
-        path.join(__dirname, "../../views/layouts/iframe.hbs"),
-        "utf8"
-      )
+      path.join(__dirname, "../../views/layouts/iframe.hbs")
     );
   }
 
@@ -26,19 +34,8 @@ module.exports = function(app, hbs, initial) {
         process.cwd(),
         `${app.get("config").srcFolder}/${filePath}`
       );
-      let readFile;
 
-      try {
-        readFile = fs.readFileSync(fullFilePath, "utf8");
-      } catch (e) {
-        console.warn(
-          `Couldn't find file ${fullFilePath}. Is the 'srcFolder' in your styleguide.json correct?`
-        );
-      }
-
-      if (readFile) {
-        hbs.registerPartial(filePath, hbs.compile(readFile.toString()));
-      }
+      register(hbs, filePath, fullFilePath);
     }
   });
 };
