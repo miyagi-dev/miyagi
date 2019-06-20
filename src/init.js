b0b20d96e0a0669856be25696cad7bb1dae29e71
mgrsskls <mail@mgrossklaus.de>
2019-06-20 13:33:14 +0200

    moved axe-core from devDependencies to dependencies

--- src/init.js
+++ src/init.js
@@ -35,10 +35,11 @@ module.exports = config => {
     path.join(__dirname, "../views"),
     path.join(process.cwd(), app.get("config").srcFolder)
   ]);
+
   app.use(express.static(process.cwd()));
   app.use(express.static(path.join(__dirname, "../assets/js")));
   app.use(express.static(path.join(__dirname, "../assets/css")));
-  app.use(express.static(path.join(__dirname, "../node_modules/axe-core")));
+  app.use(express.static(path.join(process.cwd(), "node_modules/axe-core")));
 
   server.listen(port);
 
