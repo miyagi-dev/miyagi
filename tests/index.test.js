a261d4829091cec6f364d4b266a7076c4c60e58f
Michael Großklaus <michael@factorial.io>
2020-05-09 20:40:43 +0200

    only run headman when correct commands used

--- tests/index.test.js
+++ tests/index.test.js
@@ -18,226 +18,181 @@ describe("index", () => {
       };
     });
 
-    describe("without process.env.NODE_ENV defined", () => {
-      test("returns an error message", () => {
-        const logger = require("../lib/logger.js");
+    describe("with parseable result from headman.json", () => {
+      describe("with extension, srcFolder and engine defined in headman.json", () => {
+        test("calls lib/init with parsed config", () => {
+          const fs = require("fs");
+          const init = require("../lib/init");
+          const logger = require("../lib/logger.js");
+          logger.log = jest.fn();
+          jest.mock("../lib/init");
 
-        logger.log = jest.fn();
-        delete process.env.NODE_ENV;
-        require("../index.js");
-        expect(logger.log).toHaveBeenCalledWith(
-          "error",
-          "Please define your NODE_ENV."
-        );
-      });
-    });
-  });
+          fs.readFile = jest.fn((file, encoding, cb) => {
+            cb(
+              "",
+              '{"extension": "hbs","engine": "handlebars","srcFolder": "src/"}'
+            );
+          });
 
-  describe("with process.env.NODE_ENV defined", () => {
-    describe("with headman.json", () => {
-      describe("with parseable result from headman.json", () => {
-        describe("with extension, srcFolder and engine defined in headman.json", () => {
-          test("calls lib/init with parsed config", () => {
-            const fs = require("fs");
-            const init = require("../lib/init");
-            const logger = require("../lib/logger.js");
-            logger.log = jest.fn();
-            jest.mock("../lib/init");
+          require("../index.js");
 
-            fs.readFile = jest.fn((file, encoding, cb) => {
-              cb(
-                "",
-                '{"extension": "hbs","engine": "handlebars","srcFolder": "src/"}'
-              );
-            });
-
-            require("../index.js");
-
-            expect(init).toHaveBeenCalledWith(
-              Object.assign(
-                {
-                  projectName: "headman",
-                  srcFolder: "",
-                  build: {
-                    folder: "build",
-                  },
-                  cssFiles: [],
-                  jsFiles: [],
-                  projectName: "headman",
-                  es6Modules: false,
-                  srcFolderIgnores: [
-                    "node_modules",
-                    ".git",
-                    "package.json",
-                    "package-lock.json",
-                    "headman.json",
-                  ],
-                  validations: {
-                    html: true,
-                    accessibility: true,
-                  },
-                  reload: true,
-                  isBuild: false,
-                  isGenerator: false,
+          expect(init).toHaveBeenCalledWith(
+            Object.assign(
+              {
+                projectName: "headman",
+                srcFolder: "",
+                build: {
+                  folder: "build",
                 },
-                JSON.parse(
-                  '{"extension": "hbs","engine": "handlebars","srcFolder": "src/"}'
-                )
+                cssFiles: [],
+                jsFiles: [],
+                projectName: "headman",
+                es6Modules: false,
+                srcFolderIgnores: [
+                  "node_modules",
+                  ".git",
+                  "package.json",
+                  "package-lock.json",
+                  "headman.json",
+                ],
+                validations: {
+                  html: true,
+                  accessibility: true,
+                },
+                reload: true,
+                isBuild: false,
+                isGenerator: false,
+              },
+              JSON.parse(
+                '{"extension": "hbs","engine": "handlebars","srcFolder": "src/"}'
               )
-            );
-          });
-        });
-
-        describe("without extension defined in headman.json", () => {
-          test("it calls logger.log with the correct error msg", () => {
-            const fs = require("fs");
-            const logger = require("../lib/logger.js");
-
-            logger.log = jest.fn();
-            fs.readFile = jest.fn((file, encoding, cb) => {
-              cb("", '{"engine": "handlebars","srcFolder": "src/"}');
-            });
-
-            require("../index.js");
-
-            expect(logger.log).toHaveBeenCalledWith(
-              "error",
-              appConfig.messages.missingExtension
-            );
-          });
-
-          test("doesn't call lib/init", () => {
-            const fs = require("fs");
-            const logger = require("../lib/logger.js");
-            const init = require("../lib/init");
-            jest.mock("../lib/init");
-
-            logger.log = jest.fn();
-            fs.readFile = jest.fn((file, encoding, cb) => {
-              cb("", '{"engine": "handlebars","srcFolder": "src/"}');
-            });
-
-            require("../index.js");
-
-            expect(init).not.toHaveBeenCalled();
-          });
-        });
-
-        describe("without engine defined in headman.json", () => {
-          test("it calls logger.log with the correct error msg", () => {
-            const fs = require("fs");
-            const logger = require("../lib/logger.js");
-
-            logger.log = jest.fn();
-            fs.readFile = jest.fn((file, encoding, cb) => {
-              cb("", '{"extension": "hbs","srcFolder": "src/"}');
-            });
-
-            require("../index.js");
-
-            expect(logger.log).toHaveBeenCalledWith(
-              "error",
-              appConfig.messages.missingEngine
-            );
-          });
-
-          test("doesn't call lib/init", () => {
-            const fs = require("fs");
-            const logger = require("../lib/logger.js");
-            const init = require("../lib/init");
-            jest.mock("../lib/init");
-
-            logger.log = jest.fn();
-            fs.readFile = jest.fn((file, encoding, cb) => {
-              cb("", '{"extension": "hbs","srcFolder": "src/"}');
-            });
-
-            require("../index.js");
-
-            expect(init).not.toHaveBeenCalled();
-          });
-        });
-
-        describe("without srcFolder defined in headman.json", () => {
-          test("it calls logger.log with the correct warn msg", () => {
-            const fs = require("fs");
-            const logger = require("../lib/logger.js");
-
-            logger.log = jest.fn();
-            fs.readFile = jest.fn((file, encoding, cb) => {
-              cb("", '{"engine": "handlebars","extension": "hbs"}');
-            });
-
-            require("../index.js");
-
-            expect(logger.log).toHaveBeenCalledWith(
-              "warn",
-              appConfig.messages.missingSrcFolder
-            );
-          });
-
-          test("does call lib/init", () => {
-            const fs = require("fs");
-            const logger = require("../lib/logger.js");
-            const init = require("../lib/init");
-            jest.mock("../lib/init");
-
-            logger.log = jest.fn();
-            fs.readFile = jest.fn((file, encoding, cb) => {
-              cb("", '{"engine": "handlebars","extension": "hbs"}');
-            });
-
-            require("../index.js");
-
-            expect(init).toHaveBeenCalled();
-          });
+            )
+          );
         });
       });
 
-      describe("with headman.json not being parseable", () => {
+      describe("without extension defined in headman.json", () => {
         test("it calls logger.log with the correct error msg", () => {
           const fs = require("fs");
           const logger = require("../lib/logger.js");
 
           logger.log = jest.fn();
           fs.readFile = jest.fn((file, encoding, cb) => {
-            cb("", "unparseable");
+            cb("", '{"engine": "handlebars","srcFolder": "src/"}');
           });
 
           require("../index.js");
 
-          expect(logger.log).toHaveBeenNthCalledWith(
-            2,
+          expect(logger.log).toHaveBeenCalledWith(
             "error",
-            "headman wasn't able to parse your config file."
+            appConfig.messages.missingExtension
           );
+        });
+
+        test("doesn't call lib/init", () => {
+          const fs = require("fs");
+          const logger = require("../lib/logger.js");
+          const init = require("../lib/init");
+          jest.mock("../lib/init");
+
+          logger.log = jest.fn();
+          fs.readFile = jest.fn((file, encoding, cb) => {
+            cb("", '{"engine": "handlebars","srcFolder": "src/"}');
+          });
+
+          require("../index.js");
+
+          expect(init).not.toHaveBeenCalled();
+        });
+      });
+
+      describe("without engine defined in headman.json", () => {
+        test("it calls logger.log with the correct error msg", () => {
+          const fs = require("fs");
+          const logger = require("../lib/logger.js");
+
+          logger.log = jest.fn();
+          fs.readFile = jest.fn((file, encoding, cb) => {
+            cb("", '{"extension": "hbs","srcFolder": "src/"}');
+          });
+
+          require("../index.js");
+
+          expect(logger.log).toHaveBeenCalledWith(
+            "error",
+            appConfig.messages.missingEngine
+          );
+        });
+
+        test("doesn't call lib/init", () => {
+          const fs = require("fs");
+          const logger = require("../lib/logger.js");
+          const init = require("../lib/init");
+          jest.mock("../lib/init");
+
+          logger.log = jest.fn();
+          fs.readFile = jest.fn((file, encoding, cb) => {
+            cb("", '{"extension": "hbs","srcFolder": "src/"}');
+          });
+
+          require("../index.js");
+
+          expect(init).not.toHaveBeenCalled();
+        });
+      });
+
+      describe("without srcFolder defined in headman.json", () => {
+        test("it calls logger.log with the correct warn msg", () => {
+          const fs = require("fs");
+          const logger = require("../lib/logger.js");
+
+          logger.log = jest.fn();
+          fs.readFile = jest.fn((file, encoding, cb) => {
+            cb("", '{"engine": "handlebars","extension": "hbs"}');
+          });
+
+          require("../index.js");
+
+          expect(logger.log).toHaveBeenCalledWith(
+            "warn",
+            appConfig.messages.missingSrcFolder
+          );
+        });
+
+        test("does call lib/init", () => {
+          const fs = require("fs");
+          const logger = require("../lib/logger.js");
+          const init = require("../lib/init");
+          jest.mock("../lib/init");
+
+          logger.log = jest.fn();
+          fs.readFile = jest.fn((file, encoding, cb) => {
+            cb("", '{"engine": "handlebars","extension": "hbs"}');
+          });
+
+          require("../index.js");
+
+          expect(init).toHaveBeenCalled();
         });
       });
     });
 
-    describe("without headman.json", () => {
+    describe("with headman.json not being parseable", () => {
       test("it calls logger.log with the correct error msg", () => {
         const fs = require("fs");
         const logger = require("../lib/logger.js");
 
         logger.log = jest.fn();
         fs.readFile = jest.fn((file, encoding, cb) => {
-          cb("error", undefined);
+          cb("", "unparseable");
         });
 
         require("../index.js");
 
-        expect(logger.log).toHaveBeenCalledWith(
-          "info",
-          appConfig.messages.serverStarting
-        );
-        expect(logger.log).toHaveBeenCalledWith(
+        expect(logger.log).toHaveBeenNthCalledWith(
+          2,
           "error",
-          appConfig.messages.missingEngine
-        );
-        expect(logger.log).toHaveBeenCalledWith(
-          "error",
-          appConfig.messages.missingExtension
+          "headman wasn't able to parse your config file."
         );
       });
     });
