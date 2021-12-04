2df502d3111d08062bd7dcda1351870a2950cb90
Michael Großklaus <mail@mgrossklaus.de>
2021-12-04 10:30:15 +0100

    chore(core): replaced dependency chalk with picocolors to reduce filesize

--- packages/core/lib/logger.js
+++ packages/core/lib/logger.js
@@ -6,7 +6,7 @@
  * @param {string} message
  */
 
-const chalk = require("chalk");
+const colors = require("picocolors");
 
 module.exports = function log(type, message) {
   if (process.env.MIYAGI_JS_API) return;
@@ -21,16 +21,16 @@ module.exports = function log(type, message) {
     .padStart(2, "0")} ${date.getHours()}:${date.getMinutes()}`;
 
   if (type === "error") {
-    console.error(`${chalk.grey(dateStr)} ${chalk.red("Error:")} ${message}`);
+    console.error(`${colors.gray(dateStr)} ${colors.red("Error:")} ${message}`);
   } else if (type === "warn") {
     console.info(
-      `${chalk.grey(dateStr)} ${chalk.yellow("Warning:")} ${message}`
+      `${colors.gray(dateStr)} ${colors.yellow("Warning:")} ${message}`
     );
   } else if (type === "success") {
     console.info(
-      `${chalk.grey(dateStr)} ${chalk.green("Success:")} ${message}`
+      `${colors.gray(dateStr)} ${colors.green("Success:")} ${message}`
     );
   } else {
-    console.info(`${chalk.grey(dateStr)} ${chalk.cyan("Info:")} ${message}`);
+    console.info(`${colors.gray(dateStr)} ${colors.cyan("Info:")} ${message}`);
   }
 };
