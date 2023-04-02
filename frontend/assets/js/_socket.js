fe61109e3a4fab7cab6c53dc0644626afac42449
Michael Großklaus <mail@mgrossklaus.de>
2023-04-02 22:57:16 +0200

    feat: Use ws instead of socket.io to reduce package size

--- frontend/assets/js/_socket.js
+++ frontend/assets/js/_socket.js
@@ -1,20 +1,9 @@
-/* globals io */
+const ws = new WebSocket(`ws://${document.location.host}`);
 
-/**
- * @param {boolean} reloadParent - defines if the parent window instead of the current window should be reloaded
- */
-function fileChangedCallback(reloadParent) {
-	setTimeout(() => {
-		if (reloadParent) {
-			parent.window.location.reload();
-		} else {
-			window.location.reload();
-		}
-	}, 500);
-}
-
-if (typeof io !== "undefined") {
-	const socket = io.connect(window.location.origin);
-
-	socket.on("fileChanged", fileChangedCallback);
-}
+ws.onmessage = async (message) => {
+	if (message.data === "reloadParent") {
+		parent.window.location.reload();
+	} else {
+		window.location.reload();
+	}
+};
