20f9be63c8e3012d51ac3d4b1ec0ea726ff3a028
Michael Großklaus <mail@mgrossklaus.de>
2023-07-06 19:01:44 +0200

    fix: Fixes axe reporting issue by miyagi

--- frontend/assets/js/iframe.axe.js
+++ frontend/assets/js/iframe.axe.js
@@ -1,7 +1,24 @@
 /* globals axe */
 
-axe.run(document.body.children, function (err, results) {
-	window.dispatchEvent(
-		new CustomEvent("axe:done", { detail: { err, results } })
-	);
-});
+{
+	const iframe = document.querySelector(".MiyagiComponentView-iframe");
+
+	if (iframe) {
+		iframe.contentWindow.addEventListener("load", () => {
+			iframe.contentWindow.axe.run(
+				iframe.contentWindow.document.body,
+				function (err, results) {
+					window.dispatchEvent(
+						new CustomEvent("axe:done", { detail: { err, results } })
+					);
+				}
+			);
+		});
+	} else {
+		axe.run(document.body.children, function (err, results) {
+			window.dispatchEvent(
+				new CustomEvent("axe:done", { detail: { err, results } })
+			);
+		});
+	}
+}
